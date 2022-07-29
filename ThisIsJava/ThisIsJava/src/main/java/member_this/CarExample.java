bb488df2e87df78385a04ad4becfa7a1c15f2d3f
khsdev22 <khsdev22@gmail.com>
2022-07-29 16:30:46 +0900

    static

---
+++ ThisIsJava/ThisIsJava/src/main/java/member_this/CarExample.java
@@ -0,0 +1,14 @@
+package member_this;
+
+public class CarExample {
+	
+	public static void main(String[] args) {
+		Car myCar = new Car("Æ÷¸£½¦");
+		Car yourCar = new Car("º¥Ã÷");
+		
+		myCar.run();
+		System.out.println();
+		yourCar.run();
+	}
+
+}
---
+++ ThisIsJava/ThisIsJava/src/main/java/method_overloading/Calculator.java
@@ -0,0 +1,11 @@
+package method_overloading;
+
+public class Calculator {
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+}
