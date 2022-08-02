bb3a0c0bbc4616ae386f99006b4ac33384a4af50
khsdev22 <khsdev22@gmail.com>
2022-08-02 12:44:24 +0900

    access

---
+++ ThisIsJava/ThisIsJava/src/main/java/access_field_method_02/C.java
@@ -0,0 +1,17 @@
+package access_field_method_02;
+
+import access_field_method_01.A;
+
+public class C {
+	public C() {
+		A a = new A();
+	
+	a.field1 = 1;
+//	a.field2 = 1;
+//	a.field3 = 1;
+	
+	a.method1();
+//	a.method2();
+//	a.method3();
+
+}}
