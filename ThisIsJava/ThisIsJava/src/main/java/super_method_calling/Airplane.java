e4d68f9209a8457ea342f837dd7fc7e54ea084b3
khsdev22 <khsdev22@gmail.com>
2022-08-04 10:23:57 +0900

    calling

---
+++ ThisIsJava/ThisIsJava/src/main/java/super_method_calling/Airplane.java
@@ -0,0 +1,15 @@
+//[Airplan.java] super 변수
+
+package super_method_calling;
+
+public class Airplane {
+	public void land() {
+		System.out.println("착륙합니다.");
+	}
+	public void fly() {
+		System.out.println("일반비행합니다.");
+	}
+	public void takeOff() {
+		System.out.println("이륙합니다.");
+	}
+}
