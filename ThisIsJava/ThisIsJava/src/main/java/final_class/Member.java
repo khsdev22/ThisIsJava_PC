7fa4a8f25b5a6005beb37cac25fb700b96504ebf
khsdev22 <khsdev22@gmail.com>
2022-08-05 08:06:12 +0900

    final_class}

---
+++ ThisIsJava/ThisIsJava/src/main/java/final_class/Member.java
@@ -0,0 +1,7 @@
+// [Member.java] 상속할 수 없는 final 클래스
+
+package final_class;
+
+public final class Member {
+
+}
---
+++ ThisIsJava/ThisIsJava/src/main/java/final_class/VeryVeryImportantPerson.java
@@ -0,0 +1,7 @@
+// [VeryVeryImportantPerson.java] 상속할 수 없는 final 클래스
+
+package final_class;
+
+public class VeryVeryImportantPerson extends Member {
+
+}
